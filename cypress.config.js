a857e78d17b08c54ccbde989269f8a2eeba7d10a
Jimmy Defains <jimmydef@outlook.fr>
2024-05-31 23:20:41 +0200

    fix: correct css access

---
+++ cypress.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  projectId: "au1inu",
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+  },
+});
