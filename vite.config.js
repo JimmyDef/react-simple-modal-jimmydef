03c34494edb4d2d00ef5202f65f399328728751a
Jimmy Defains <jimmydef@outlook.fr>
2024-06-01 16:32:47 +0200

    Fix: vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,15 @@
 import { defineConfig } from "vite";
-import { extname, relative, resolve } from "path";
-import { fileURLToPath } from "url";
-import { glob } from "glob";
 import react from "@vitejs/plugin-react";
-
-const inputFiles = glob.sync("src/lib/**/*.{js,jsx}");
-console.log("Found input files:", inputFiles);
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 export default defineConfig({
   css: {
     modules: true,
-    scopeBehaviour: "local",
   },
   optimizeDeps: {
     include: ["react-simple-modal-jimmydef/dist/style.css"],
   },
-  plugins: [react()],
+  plugins: [react(), libInjectCss()],
   test: {
     environment: "jsdom",
     globals: true,
@@ -26,17 +20,14 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry:
-        inputFiles.length > 0
-          ? resolve(inputFiles[0])
-          : "./src/lib/components/Modal.jsx",
+      entry: "./src/lib/components/Modal.jsx",
       name: "ReactSimpleModaljimmydef",
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
       external: ["react", "react-dom"],
-      input: inputFiles.map((file) => resolve(file)),
       output: {
+        chunkFileNames: "chunks/[name].[hash].js",
         assetFileNames: "assets/[name][extname]",
         entryFileNames: "[name].js",
         globals: {
